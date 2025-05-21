b2f5e94d9dd84b75f8b07617512abadc558eac34
fromancient <joinercantillo0813@gmail.com>
2025-05-21 10:55:20 -0400

    update the payment logic

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import PaymentScreen from '@/components/PaymentScreen';
 
 // Update these to match your .env.local file
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
