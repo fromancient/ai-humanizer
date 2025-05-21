4dc3b61197ef4f5d8b757c9043b3cfb6cc8c39f0
fromancient <joinercantillo0813@gmail.com>
2025-05-21 10:29:22 -0400

    add env variable example

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -4,4 +4,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient("https://dtpumxlabvahvsymhgye.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cHVteGxhYnZhaHZzeW1oZ3llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3ODcxNjEsImV4cCI6MjA2MzM2MzE2MX0.0af6aDA8NV8H5qrUPBxG8TmAaAhPrxjdCq9EAhEcWhA"); 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
